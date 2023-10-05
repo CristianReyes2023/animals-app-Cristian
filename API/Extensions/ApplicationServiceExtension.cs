b215bae6311ba8607a192c90660d00cd6a9cb88b
“CristianReyes2023” <cristianlrm2019@gmail.com>
2023-10-04 22:48:03 -0500

    feat: :sparkles: ApplicationServiceExtension

---
+++ API/Extensions/ApplicationServiceExtension.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AspNetCoreRateLimit;
+
+namespace API.Extensions;
+public static class ApplicationServiceExtension //Agregar el STATIC   IMPORTANTE
+{
+    public static void ConfigureCors(this IServiceCollection services) => services.AddCors(options =>
+    {
+        options.AddPolicy("CorsPolicy", builder =>
+        {
+            builder.AllowAnyOrigin() // WithOrigins("https://domain.com")
+            .AllowAnyMethod() // WithMethods("GET", "POST")
+            .AllowAnyHeader(); // WithHeaders("accept", "content-type")
+        });
+    }); // Remember to put 'static' on the class and to add builder.Services.AddApplicationServices(); to Program.cs and builder.Services.ConfigureCors(); and app.UseCors("CorsPolicy");
+    
+    // public static void AddApplicationServices(this IServiceCollection services)
+    // {
+    //     services.AddScoped<IUnitOfWork, UnitOfWork>();
+    // }
+    public static void ConfigurationRatelimiting (this IServiceCollection services)
+    {//Vamos a poder determinar en que momento vamos a establecer ese rate limit atraves de los encabezados en un expresion y poder determinar dentro de header de la petición cuantas nos quedan disponibles.
+        services.AddMemoryCache(); //Aministrar primero memoria cache
+        services.AddSingleton<IRateLimitConfiguration,RateLimitConfiguration>();//es un método que se utiliza para registrar una implementación específica de una interfaz o una clase con el contenedor de inyección de dependencias. La implementación registrada se crea una vez y se reutiliza en toda la aplicación cada vez que se necesita.
+        services.AddInMemoryRateLimiting();//Argumentos y metodos que permiten almacer información en memoria de manera temporal
+        services.Configure<IpRateLimitOptions>(options =>//aca determino cuales son las diferentes opciones que va a tener ese limite
+        {
+            options.EnableEndpointRateLimiting =true;
+            options.StackBlockedRequests = false;
+            options.HttpStatusCode = 429;
+            options.RealIpHeader = "X-Real-IP";
+            options.GeneralRules = new List<RateLimitRule>
+            {
+                new RateLimitRule
+                {//Algo importante que tenemos en el rate limit es está configuración que tenemos acontinuación
+                Endpoint = "*",
+                Period = "10s",
+                Limit = 2
+                }
+            };
+        });
+    }
+}
--- API/Program.cs
+++ API/Program.cs
@@ -1,3 +1,6 @@
+using System.Reflection;
+using API.Extensions;
+using AspNetCoreRateLimit;
 using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 
@@ -6,6 +9,9 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.ConfigurationRatelimiting();//
+builder.Services.ConfigureCors();
+builder.Services.AddAutoMapper(Assembly.GetEntryAssembly());//Es propiamente de C#, el Assembly.GetEntreAssembly se utiliza para poder obtener la ruta de donde se está ejecutan el ensamblado el DLL en este caso el proyecto.
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -20,12 +26,15 @@ var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
-{
+{ 
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+//Se necesita para completar nuestra configuración de dependencia, llamamos nuestra configuración de Cors y Ip en este caso. Y eso lo hacemos atraves niderwork
 
+app.UseCors("CorsPolicy");
 app.UseHttpsRedirection();
+app.UseIpRateLimiting();
 
 app.UseAuthorization();
 
